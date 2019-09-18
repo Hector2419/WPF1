5aa76ef11951dbff72b6f77af0ed3e6dcc5b9756
Alumno <Alumno@PC17>
2019-09-18 12:32:19 -0500

    Lel

---
+++ 180919/180919/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _180919
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Login con puerta abierta";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Iniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if (nombre.Text=="juancho")
+            {
+                if (contrasena.Text=="123tamarindo")
+                {
+                    Window1 form = new Window1();
+                    form.ShowDialog();
+                     
+                }
+                else
+                {
+                    MessageBox.Show("Su nombre de usuario o su contraseña son incorrectos.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Su nombre de usuario o su contraseña son incorrectos.");
+            }
+        }
+    }
+}
